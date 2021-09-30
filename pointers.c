6dbebf4e4e5df36b51daef6b07ea4b8da8b331e5
Ca7Ac1 <Ca7Ac1@gmail.com>
2021-09-29 21:26:23 -0400

    Finished work

--- pointers.c
+++ pointers.c
@@ -5,7 +5,7 @@ int main()
 	char c = 'a';
 	int i = 1;
 	long l = 70000;
-	
+
 	printf("char: %c\nhex: %x\ndecimal: %lu\n\n", c, &c, &c);
 	printf("int: %d\nhex: %x\ndecimal: %lu\n\n", i, &i, &i);
 	printf("long: %lu\nhex: %x\ndecimal: %lu\n\n", l, &l, &l);
@@ -26,14 +26,28 @@ int main()
 	printf("char: %c\n", c);
 	printf("int: %d\n", i);
 	printf("long: %lu\n\n", l);
-	
+
 	unsigned int ui = 700000000;
 	int *rightPointer = &ui;
 	char *wrongPointer = &ui;
-	
+
+	printf("unsigned int:\nint ptr: %p %u\nchar ptr: %p %u\n\n", rightPointer, *rightPointer, wrongPointer, *wrongPointer);
 	printf("unsigned int: \n%x\n%u\n\n", ui, ui);
-	
-	printf("bytes of unsigned int: \n%p\n%p\n%p\n%p\n\n", wrongPointer, 
-	++wrongPointer, ++wrongPointer, ++wrongPointer);
+
+	printf("bytes of unsigned int: \n%hhu\n%hhu\n%hhu\n%hhu\n\n", *wrongPointer,
+		   wrongPointer[1], wrongPointer[2], wrongPointer[3]);
+
+	wrongPointer[0]++;
+	wrongPointer[1]++;
+	wrongPointer[2]++;
+	wrongPointer[3]++;
+
+	printf("unsigned int after modification: \n%x\n%u\n\n", ui, ui);
+
+	wrongPointer[0] += 16;
+	wrongPointer[1] += 16;
+	wrongPointer[2] += 16;
+	wrongPointer[3] += 16;
+
 	return 0;
 }
